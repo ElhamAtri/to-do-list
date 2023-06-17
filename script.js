0ed32e2abef345f3b74a8885ba203ccf70b3eec3
elham atri <elhmatz@gmail.com>
2023-06-16 17:50:40 -0700

    add project

--- script.js
+++ script.js
@@ -1,60 +1,37 @@
-/* --------------- Dynamic Greetings  --------------------- */
 let greetings = document.querySelector("#greetings");
-
 let liveTime = new Date().getHours();
 let greeting = liveTime >= 5 && liveTime < 12 ? "Good Morning<3" : 
 liveTime >= 12 && liveTime < 18 ? "Good Afternoon<3" :
 liveTime >= 18 && liveTime < 21 ? "Good Evening<3" : "Good Night<3";
-
 greetings.innerHTML = greeting;
-/* --------------- End Dynamic Greetings  --------------------- */
-/* --------------- Date Time  --------------------- */
-
-/* --------------- End Date Time  --------------------- */
-
-/* --------------- To Do Web App  --------------------- */
-
-/*  Create Vars   */
 const newTaskInput = document.querySelector("#create_task input");
 const tasksDiv = document.querySelector("#todo_list");
 const deleteAll = document.querySelector("#delete_all");
 let deleteTasks, editTasks, tasks;
 let updateNote = "";
 let count;
-
-/*  When Window Load  */
 window.onload = () => {
   updateNote = "";
   count = Object.keys(localStorage).length;
   displayTasks();
 };
-
-/* Display To-do List   */
 const displayTasks = () => {
   if (Object.keys(localStorage).length > 0) {
     tasksDiv.style.display = "inline-block";
   } else {
     tasksDiv.style.display = "none";
   }
-
-/* Display Delete All Button   */
   if (Object.keys(localStorage).length > 1) {
     deleteAll.style.display = "block";
   } else {
     deleteAll.style.display = "none";
   }
-
-  /*  Clear To-do List   */
   tasksDiv.innerHTML = "";
-
-  /*  Fetch Keys in local storage   */
   let tasks = Object.keys(localStorage);
   tasks = tasks.sort();
 
   for (let key of tasks) {
-    let classValue = "";
-
-    /*  Get All Values   */  
+    let classValue = ""; 
     let value = localStorage.getItem(key);
     let taskInnerDiv = document.createElement("div");
     taskInnerDiv.classList.add("task_list");
@@ -73,8 +50,6 @@ const displayTasks = () => {
     taskInnerDiv.innerHTML += `<button class="delete_task"><i class="fa-solid fa-trash"></i></button>`;
     tasksDiv.appendChild(taskInnerDiv);
   }
-
-  /*  Task Completed   */
   tasks = document.querySelectorAll(".task_list");
   tasks.forEach((element, index) => {
     element.onclick = () => {
@@ -85,8 +60,6 @@ const displayTasks = () => {
       }
     };
   });
-
-  /*  Edit Task   */
   editTasks = document.getElementsByClassName("edit_task");
   Array.from(editTasks).forEach((element, index) => {
     element.addEventListener("click", (e) => {
@@ -98,8 +71,6 @@ const displayTasks = () => {
       parent.remove();
     });
   });
-  
-  /*  Delete Task  */
   deleteTasks = document.getElementsByClassName("delete_task");
   Array.from(deleteTasks).forEach((element, index) => {
     element.addEventListener("click", (e) => {
@@ -111,34 +82,24 @@ const displayTasks = () => {
     });
   });
 };
-
-/*  Disable Edit Button   */
 const disableButtons = (bool) => {
   let editButtons = document.getElementsByClassName("edit_task");
   Array.from(editButtons).forEach((element) => {
     element.disabled = bool;
   });
 };
-
-/*  Delete Task From Local Storage   */
 const removeTask = (taskValue) => {
   localStorage.removeItem(taskValue);
   displayTasks();
 };
-
-/*  Delete All From Local Storage   */
 document.querySelector("#delete_all").addEventListener("click", () => {
   localStorage.clear();
   displayTasks();
 });  
-
-/*  Add Task To Local Storage   */
 const updateStorage = (index, taskValue, completed) => {
   localStorage.setItem(`${index}_${taskValue}`, completed);
   displayTasks();
 };
-
-/*  Add New Task   */
 document.querySelector("#add_task").addEventListener("click", () => {
   disableButtons(false);
   if (newTaskInput.value.length == 0) {
@@ -156,5 +117,3 @@ document.querySelector("#add_task").addEventListener("click", () => {
     newTaskInput.value = "";
   }
 });  
-
-/* --------------- End To Do Web App  --------------------- */
